8ef8a178c2b8f6ced80839ef0797388c233d303f
islam khaled <khaledislam9003@gmail.com>
2025-04-19 14:00:16 +0200

    updata

--- public/javascripts/showpageMap.js
+++ public/javascripts/showpageMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: campground,
+    center: campground.geometry.coordinates,
     zoom: 8
 });
 
@@ -10,11 +10,11 @@ map.addControl(new mapboxgl.NavigationControl());
 
 
 new mapboxgl.Marker()
-    .setLngLat(campground)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${tit.title}</h3><p>${loc}</p>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map);
